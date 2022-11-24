9ef9b366d342089e544f64280b5707b4dfe4df28
Elthon <eltonsilva.mecatronic@gmail.com>
2022-11-23 21:59:50 -0300

    adicionando funcionalidades

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,12 +1,23 @@
 import { Request, Response } from 'express';
-import { Todo } from '../models/Todo';
+import { Todo }  from '../models/Todo';
 
-export const all = async () => {
-
+export const all = async (req: Request, res: Response) => {
+    const list = await Todo.findAll();
+    res.json({ list });
 }
 
-export const add = async () => {
-    
+export const add = async (req: Request, res: Response) => {
+    if(req.body.title) {
+        let newTodo = await Todo.create({
+            title: req.body.title,
+            done: req.body.done ? true : false
+        });
+
+        res.status(201).json({ item: newTodo });
+
+    } else {
+        res.json({ error: 'Dados não enviados.' });
+    }
 }
 
 export const update = async () => {
